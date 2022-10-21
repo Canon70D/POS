1d4bd02172a7fd938d06fce151d9ce96eb43daa9
FEIYU YIN <105698932+Canon70D@users.noreply.github.com>
2022-10-21 14:37:24 +1030

    set up folders for server and update server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+require("colors");
+const path = require("path");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
+
+// app.use(routes);
+app.get("/", (req, res) => {
+  res.send("pos server");
+});
+
+// db.once("open", () => {
+//   app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
+// });
+
+app.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+});
