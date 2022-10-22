8cd4cfbe55ba08c253433a7792eec9331d074d15
FEIYU YIN <105698932+Canon70D@users.noreply.github.com>
2022-10-22 15:09:53 +1030

    two pages built for react route  test

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import "antd/dist/antd.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
-    <div>
-      <h1>POS SYSTEM</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/products" element={<ProductPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
